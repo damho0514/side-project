9a79ad4ae78f2d8c8efd3a710ccc878487bc7932
baedamho <damho0514@mondrian.ai>
2023-01-19 18:58:10 +0900

    test:

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import { initMocks } from './test/server';
+
+// initMocks();
 
 ReactDOM.render(
   <React.StrictMode>
